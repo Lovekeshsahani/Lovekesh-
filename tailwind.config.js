00ef29b5f08e2004a9ea8673e66ead8d659ade52
Lovekesh sahani <sangeetasahani69@gmail.com>
2026-08-17 15:23:12 +0530

    Initial commit: Gana Bajao - Professional Music Streaming App

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1DB954',
+        secondary: '#191414',
+        accent: '#1ed760',
+      },
+      backgroundColor: {
+        base: '#121212',
+        surface: '#181818',
+        hover: '#282828',
+      },
+    },
+  },
+  plugins: [],
+};
